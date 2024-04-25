e1673eb66d60f535ddc26ec42439f83c13d3e882
Brian Ward <bward@flatironinstitute.org>
2024-04-25 16:51:27 -0400

    first commit

---
+++ dummy.cpp
@@ -0,0 +1,16 @@
+// file dummy.cpp
+#include <iostream>
+
+extern "C" int very_long(
+    const void *model, size_t num_paths, const char *inits,
+    unsigned int seed, unsigned int id, double init_radius, int num_draws,
+    int max_history_size, double init_alpha, double tol_obj,
+    double tol_rel_obj, double tol_grad, double tol_rel_grad, double tol_param,
+    int num_iterations, int num_elbo_draws, int num_multi_draws,
+    bool calculate_lp, bool psis_resample, int refresh, int num_threads,
+    double *out, size_t out_size, void **err)
+{
+
+    std::cout << num_threads << std::endl;
+    return 1;
+}
---
+++ dummy.jl
@@ -0,0 +1,58 @@
+run(`g++ dummy.cpp -shared -fPIC -o dummy.so`)
+
+dummy = Libc.Libdl.dlopen("./dummy.so")
+
+out = [0.0]
+ccall(Libc.Libdl.dlsym(dummy, "very_long"),
+    Cint,
+    (
+        Ptr{Cvoid},
+        Csize_t,
+        Cstring,
+        Cuint,
+        Cuint,
+        Cdouble,
+        Cint,
+        Cint,
+        Cdouble,
+        Cdouble,
+        Cdouble,
+        Cdouble,
+        Cdouble,
+        Cdouble,
+        Cint,
+        Cint,
+        Cint,
+        Cint, # really bool
+        Cint, # really bool
+        Cint,
+        Cint,
+        Ref{Cdouble},
+        Csize_t,
+        Ref{Ptr{Cvoid}},
+    ),
+    C_NULL,
+    1,
+    "dummy",
+    1234,
+    1,
+    2.0,
+    1000,
+    5,
+    0.1,
+    0.1,
+    0.1,
+    0.1,
+    0.1,
+    0.1,
+    1000,
+    1000,
+    1000,
+    Int32(true),
+    Int32(false),
+    0,
+    Int32(-1), # THIS IS THE ONE THAT GETS PRINTED
+    out,
+    length(out),
+    C_NULL,
+)
