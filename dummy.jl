f0fa0e0235a461bf90cad1a37c69920e22ae8ec2
Mosè Giordano <mose@gnu.org>
2024-04-25 23:12:47 +0100

    Simplify `ccall`

--- dummy.jl
+++ dummy.jl
@@ -1,64 +1,34 @@
 run(`gcc dummy.c -shared -fPIC -o dummy.so`)
 
-dummy = Libc.Libdl.dlopen("./dummy.so")
-
-
 num_threads = -1
 println("in Julia:\t", num_threads)
 
 out = [0.0]
-ret = ccall(Libc.Libdl.dlsym(dummy, "very_long"),
-    Cint,
-    (
-        Ptr{Cvoid},
-        Csize_t,
-        Cstring,
-        Cuint,
-        Cuint,
-        Cdouble,
-        Cint,
-        Cint,
-        Cdouble,
-        Cdouble,
-        Cdouble,
-        Cdouble,
-        Cdouble,
-        Cdouble,
-        Cint,
-        Cint,
-        Cint,
-        Bool, # really bool
-        Bool, # really bool
-        Cint,
-        Cint,
-        Ref{Cdouble},
-        Csize_t,
-        Ref{Ptr{Cvoid}},
-    ),
-    C_NULL,
-    1,
-    "dummy",
-    1234,
-    1,
-    2.0,
-    1000,
-    5,
-    0.1,
-    0.1,
-    0.1,
-    0.1,
-    0.1,
-    0.1,
-    1000,
-    1000,
-    1000,
-    (true),
-    (false),
-    0,
-    num_threads, # THIS IS THE ONE THAT GETS PRINTED
-    out,
-    length(out),
-    C_NULL,
-)
+ret = @ccall "./dummy.so".very_long(
+    C_NULL::Ptr{Cvoid},
+    1::Csize_t,
+    "dummy"::Cstring,
+    1234::Cuint,
+    1::Cuint,
+    2.0::Cdouble,
+    1000::Cint,
+    5::Cint,
+    0.1::Cdouble,
+    0.1::Cdouble,
+    0.1::Cdouble,
+    0.1::Cdouble,
+    0.1::Cdouble,
+    0.1::Cdouble,
+    1000::Cint,
+    1000::Cint,
+    1000::Cint,
+    true::Bool,  # really bool
+    false::Bool, # really bool
+    0::Cint,
+    num_threads::Cint, # THIS IS THE ONE THAT GETS PRINTED
+    out::Ref{Cdouble},
+    length(out)::Csize_t,
+    C_NULL::Ref{Ptr{Cvoid}},
+)::Cint
 
 println("in Julia:\t", ret)
